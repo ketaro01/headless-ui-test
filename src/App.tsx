038b0e7d284383fad5abb542f069ede6a3355a77
liamyoon <liam.yoon@nexr.com>
2022-02-11 10:36:53 +0900

    정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import React, {useState} from 'react';
-import { genTestData } from './useTestTable/genTestData';
+import React from 'react';
 import Example from './Example';
 
 function App() {
-  const [testData] = useState(genTestData({
-    id: 'index',
-    switch: 'boolean',
-    userName: 'name',
-    age: 'number:20,30',
-    place: 'place',
-    date: 'date:YYYY-MM-DD',
-    value: 'fixValue',
-  })(200));
-
   return (
     <div className="App">
       <Example />
